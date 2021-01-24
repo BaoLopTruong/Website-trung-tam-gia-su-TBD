01f01968594fc6e6ebdc1e3b47102d95614be91b
kiuozero <vducvtc@gmail.com>
2021-01-24 08:50:51 +0700

    ok commit

---
+++ TBD/Controler/connect.php
@@ -0,0 +1,20 @@
+<?php 
+    // $db_host = "localhost";
+    // $db_username = "root";
+    // $db_password = "";
+    // $db_name = "quanlygiasu";
+
+    // $info['db_host'] = "localhost";
+    // $info['db_name'] = "quanlygiasu";
+    // $info['db_username'] = "root";
+    // $info['db_password'] = "";
+
+    // $conn = @mysqli_connect($db_host, $db_username, $db_password) or die("Không thể kết nối database");
+    // $db = @mysqli_select_db($db_name,$conn) or die("Không thể chọn database");
+    $ketnoi['host'] = 'localhost'; 
+    $ketnoi['dbname'] = 'quanlygiasu'; // Tên database 
+    $ketnoi['username'] = 'root'; // Tên user mặc định là root 
+    $ketnoi['password'] = ''; // Password để trống 
+    $conn = @mysqli_connect( "{$ketnoi['host']}", "{$ketnoi['username']}", "{$ketnoi['password']}") or die("Không thể kết nối database");
+    @mysqli_select_db( $conn,"{$ketnoi['dbname']}") or die("Không thể chọn database"); 
+?>
--- TBD/Controler/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if( !isset($_SESSION["user"])){
-        header("location:login.php");
-    }
-?>
-<h1>Yêu cầu đăng nhập để trang hoạt động</h1>
-<a href="logout.php" title="logout">Logout</a>
